a479e0e75266d4ee13e7bceca5a12e902aa915a7
MyNameVision <sitaramkadam.tester@gamil.com>
2026-05-13 10:58:17 +0530

    Check String Pelindrome Program

--- FastBilling/src/MyPackage/RevString.java
+++ FastBilling/src/MyPackage/RevString.java
@@ -2,12 +2,22 @@ package MyPackage;
 
 public class RevString {
     public static void main(String[] args) {
-        String str = "Hello World";
+
+        // revers the String
+        String str = "MADAM";
         String rev = "";
         for (int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
         }
         
         System.out.println("Reversed String: " + rev);
+ 
+        
+        // String is Pelindrome or Not
+        if (str.equals(rev)) {
+            System.out.println("String is Palindrome");
+        } else {
+            System.out.println("String is Not Palindrome");
+        }
     }
 }
